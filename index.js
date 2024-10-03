39a74a88dbf2515b7e17b72e9909b61978dbb2d2
habibium <habib.prodev@gmail.com>
2024-10-04 04:40:34 +0600

    fix: handle absence of named route parameter for /api/:date

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+app.get("/api", function (req, res) {
+  console.log("REQUEST PARAMS:", req.params);
+  const now = new Date();
+  return res.json({ unix: now.getTime(), utc: now.toUTCString() });
+});
+
 app.get("/api/:date", function (req, res) {
   console.log("REQUEST PARAMS:", req.params);
   const unix = +req.params?.date;
